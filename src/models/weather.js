a843e41bff2ab84b9b507d8b664ebd9423d45e3e
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-17 08:02:49 +0530

    feat: integrate src/models/weather.js

---
+++ src/models/weather.js
@@ -0,0 +1,8 @@
+class Weather {
+    constructor(date, temperature, condition) {
+        this.date = date;
+        this.temperature = temperature;
+        this.condition = condition;
+    }
+}
+module.exports = Weather;
